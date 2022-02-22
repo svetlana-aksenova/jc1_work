c08281bc7beaf095593ae2d52f5c857e033e6152
svetlana-aksenova <svetlana.aksenova.v>
2022-02-22 21:27:37 +0300

    Class Car

--- src/Hello.java
+++ src/Hello.java
@@ -1,13 +1,31 @@
-public class Hello{
-
-    String nameOfTheMan = "Иван";
-    int height = 186;
-
-	public static void main(String[] args) {
-
-	    System.out.println("Hello World " + args[0]);
+public class Hello {
 
 
-	    
-	}
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hello World " + args[0]);
+
+        Car myCar;
+        myCar=new Car();
+        new Car();
+        myCar.setPrice(20_000_000);
+        System.out.println(myCar.price);
+        System.out.println(myCar.model);
+        System.out.println(myCar.getModel().length());
+    }
 }
+	class Car{
+        int price;
+        String model = "BMW X6";
+    public int getPrice(){
+        return price;
+
+    }
+    public void setPrice(int newPrice){
+        price = newPrice;
+    }
+    public String getModel(){
+        return model;
+    }
+
+    }
