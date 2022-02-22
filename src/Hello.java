b44867df2ba508b0440b061871447023910ec33d
svetlana-aksenova <svetlana.aksenova.v>
2022-02-22 19:10:38 +0300

    Initial commit of Hello World java program

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+
+    String nameOfTheMan = "Иван";
+    int height = 186;
+
+	public static void main(String[] args) {
+
+	    System.out.println("Hello World " + args[0]);
+
+
+	    
+	}
+}
